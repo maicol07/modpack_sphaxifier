d8f193e10c14788b7563cd43fe38c400e91b0a06
Maicol <maicolbattistini@live.it>
2020-09-15 15:42:55 +0200

    build: 🔨 Improved build scripts
    - fix: App info not showing in compiled exe

--- build.bat
+++ build.bat
@@ -1,11 +1,11 @@
 rem Pyinstaller
 pyinstaller -y ^
---add-data VERSION;VERSION ^
+--add-data VERSION;. ^
 --add-data resources/html;resources/html ^
 --add-data resources/KOMIKAX_.ttf;resources/KOMIKAX_.ttf ^
 --add-data resources/img;resources/img ^
 --add-data resources/views;resources/views ^
---add-data C:\Users\Maicol\.conda\envs\Modpack_Sphaxifier\Lib\site-packages\pyside_material\themes;pyside_material\themes ^
+--add-data C:\Users\Maicol\AppData\Local\Programs\Python\Python38\Lib\site-packages\pyside_material\themes;pyside_material\themes ^
 -i resources/img/logo.ico ^
 --hidden-import six ^
 --hidden-import PySide2.QtXml ^
